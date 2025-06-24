7c34f2a9bfffacffce03053118522442421764c6
burningskull98 <Burningskull@mail.ru>
2025-06-24 20:03:37 +0700

    Initial commit

---
+++ shop_app/views.py
@@ -0,0 +1,67 @@
+"""
+Этот модуль отвечает за обработку пользовательских запросов и отображение данных в приложении.
+"""
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.shortcuts import render
+from .models import Product
+from .forms import ProductModelForm
+
+
+
+def index(request):
+    """
+        Обрабатывает запросы к главной странице приложения.
+        """
+    return render(request, 'shop_app/base.html')
+
+
+class ProductListView(ListView):
+    """
+       Представление для отображения списка продуктов.
+      """
+    model = Product
+    template_name = 'shop_app/product_list.html'
+    context_object_name = 'products'
+
+
+class ProductDetailView(DetailView):
+    """
+    Представление для отображения подробной информации о продукте.
+   """
+    model = Product
+    template_name = 'shop_app/product_detail.html'
+    context_object_name = 'product'
+
+
+class ProductCreateView(CreateView):
+    """
+         Представление для создания продукта.
+         """
+    model = Product
+    template_name = 'shop_app/create_product.html'
+    form_class = ProductModelForm
+    success_url = reverse_lazy('products')
+
+
+class ProductUpdateView(UpdateView):
+    """
+         Представление для редактирования продукта.
+         """
+    model = Product
+    template_name = 'shop_app/edit_product.html'
+    form_class = ProductModelForm
+    success_url = reverse_lazy('products')
+
+
+class ProductDeleteView(DeleteView):
+    """
+     Представление для удаления продукта.
+     """
+    model = Product
+    template_name = 'shop_app/delete_product'
+    form_class = ProductModelForm
+    success_url = reverse_lazy('products')
+from django.shortcuts import render
+
+# Create your views here.
